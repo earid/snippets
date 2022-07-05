629e026821cdc48650f78c22853cf7348d71709b
Earid A <earid777@gmail.com>
2022-07-05 23:45:25 +0600

    Create js-snippets.js

---
+++ js-snippets.js
@@ -0,0 +1,11 @@
+/* Snipppet 1 */
+/* slide chaneg on image */ 
+
+function imageSlider(anything){
+    document.querySelector('.pepsi').src = anything;
+  }
+
+ function changebgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+ }
