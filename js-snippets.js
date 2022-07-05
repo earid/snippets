bd3ddfa7347efb0971ee2e8cb82c85247991603a
Earid A <earid777@gmail.com>
2022-07-05 23:54:05 +0600

    Update js-snippets.js

--- js-snippets.js
+++ js-snippets.js
@@ -5,7 +5,10 @@ function imageSlider(anything){
     document.querySelector('.pepsi').src = anything;
   }
 
+HTML >> onclick="imageSlider('image/link.png');"
+
  function changebgColor(color){
     const sec = document.querySelector('.sec');
     sec.style.background = color;
  }
+HTML >> onclick="changebgColor('#FFF');"
