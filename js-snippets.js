09270de81026c8688ba6b8fbd969cbe05a9f27ec
Earid A <earid777@gmail.com>
2023-01-09 01:00:45 +0600

    Update js-snippets.js

--- js-snippets.js
+++ js-snippets.js
@@ -1,34 +1,33 @@
 /* Snipppet 1 ----- slide chaneg on image */
-
-function funName(funParam){
+function funName(funParam) {
     document.querySelector('.demo').src = funParam;
-  }
+}
 
-HTML >> onclick="funParam('image/link.png');"
+HTML >> onclick = "funParam('image/link.png');"
 
 /* Snipppet 2 ----- change bg color */
 
- function funName(funParam){
+function funName(funParam) {
     const sec = document.querySelector('.sec');
     sec.style.background = funParam;
- }
-HTML >> onclick="funName('#FFF');"
+}
+HTML >> onclick = "funName('#FFF');"
 
 /* Snipppet 3 ----- insert a class with jQuery / Menu toggle*/
 
- function toggleMenu(){
-            var menuToggle = document.querySelector('.toggle');
-            menuToggle.classList.toggle('active')
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    menuToggle.classList.toggle('active')
 
-/* Sticky header function */
-            window.addEventListener('scroll', function(){
-            var header = document.querySelector('header');
-            header.classList.toggle('sticky' , window.scrollY > 0)
-        });
+    /* Sticky header function */
+    window.addEventListener('scroll', function() {
+        var header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 0)
+    });
 
-/* add active class */
-        let sideBar = document.querySelector('.container .sidebar');
+    /* add active class */
+    let sideBar = document.querySelector('.container .sidebar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    sideBar.classList.toggle('active');
-};
+    document.querySelector('#menu-btn').onclick = () => {
+        sideBar.classList.toggle('active');
+    };
