df61fba27d1762b6258fb723cb4c54302fca906c
Earid A <earid777@gmail.com>
2023-05-19 02:35:49 +0600

    Create responsiveheader.js

---
+++ responsiveheader.js
@@ -0,0 +1,12 @@
+<header class="header">
+  <a href="" class="logo">marketo.</a>
+  <nav class="navbar">
+    <a href="#home">home</a>
+    <a href="#services">services</a>
+    <a href="#about">about</a>
+    <a href="#contact">contact</a>
+  </nav>
+  <div id="menu-btn" class="fas fa-bars"></div>
+</header>
+
+/* csss */
